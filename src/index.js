164ff378ef0bd2fc3e8985dfb4f56b3ade3a6781
PundrikThakkar <pundrikpundrik3@gmail.com>
2024-03-02 10:43:51 +0530

    mongodb connected

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+connectDB();
+
+/*
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Errr:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Express is running on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log("ERROR:", err);
+    throw err;
+  }
+})();
+*/
