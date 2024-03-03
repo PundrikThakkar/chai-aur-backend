ebe3272f3b8053b27429580e90244e639625565b
PundrikThakkar <pundrikpundrik3@gmail.com>
2024-03-03 11:10:01 +0530

    linked with postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,20 +19,28 @@ const registerUser = asyncHandler(async (req, res) => {
   console.log("email:", email);
 
   if (
-    [fullName, email, username, password].some((field) => field?.trime() === "")
+    [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({ $or: [{ username }, { email }] });
+  const existedUser = await User.findOne({ $or: [{ username }, { email }] });
 
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required!");
   }
